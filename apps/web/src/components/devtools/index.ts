e5c57047aef2084019351f013c2b2fdcff390a26
Žiga Krašovec <zkrasovec@gmail.com>
2025-10-07 12:47:06 +0200

    fixed devtools imports

--- apps/web/src/components/devtools/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './QueryDevTools';
-export * from './RouterDevTools';
--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,9 +1,9 @@
 import { type QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRootRouteWithContext, HeadContent, Link, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { Suspense } from 'react';
 import { Toaster } from 'sonner';
-
-import { QueryDevtools, RouterDevtools } from '@/components/devtools';
 
 function RootLayout() {
   const { queryClient } = Route.useRouteContext();
@@ -14,8 +14,8 @@ function RootLayout() {
       <Toaster position="top-center" richColors />
       <Outlet />
       <Suspense fallback={null}>
-        <QueryDevtools client={queryClient} />
-        <RouterDevtools />
+        <ReactQueryDevtools client={queryClient} />
+        <TanStackRouterDevtools />
       </Suspense>
     </>
   );
