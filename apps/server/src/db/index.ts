c56b75550a0b6127756262ed101a6f93a8a3f3fe
Žiga Krašovec <zkrasovec@gmail.com>
2025-10-09 20:55:15 +0200

    use neon-http driver

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,15 +1,16 @@
-import { Pool } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/node-postgres';
-// import { Pool as PgPool } from 'pg';
+// import { Pool } from '@neondatabase/serverless';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+// import { Pool } from 'pg';
 
 import * as schema from '@/db/schema';
 
 // const Pool = import.meta.env.PROD ? NeonPool : PgPool;
 // const Pool = PgPool;
 
-export const pool = new Pool({
-  connectionString: import.meta.env.HOMEWISE_DATABASE_URL,
-});
+// export const pool = new Pool({
+//   connectionString: import.meta.env.HOMEWISE_DATABASE_URL,
+// });
 
-export const db = drizzle(pool, { schema, casing: 'snake_case' });
+export const db = drizzle({ client: neon(import.meta.env.HOMEWISE_DATABASE_URL), schema, casing: 'snake_case' });
 export { schema };
