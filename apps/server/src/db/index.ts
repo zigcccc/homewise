77f5c1e96ebfb634d4e2c62545b5f51c49cb0948
Žiga Krašovec <zkrasovec@gmail.com>
2025-10-09 19:29:13 +0200

    debug: always use pg pool

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,15 +1,14 @@
-import { Pool as NeonPool } from '@neondatabase/serverless';
+// import { Pool as NeonPool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool as PgPool } from 'pg';
 
 import * as schema from '@/db/schema';
 
-const Pool = import.meta.env.PROD ? NeonPool : PgPool;
+// const Pool = import.meta.env.PROD ? NeonPool : PgPool;
+const Pool = PgPool;
 
 export const pool = new Pool({
   connectionString: import.meta.env.HOMEWISE_DATABASE_URL,
-  max: 10,
-  idleTimeoutMillis: 30000,
 });
 
 export const db = drizzle(pool, { schema, casing: 'snake_case' });
