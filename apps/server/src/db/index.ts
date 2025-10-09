366738604b67d564af3a8ae8b1185a417d645953
Žiga Krašovec <zkrasovec@gmail.com>
2025-10-09 20:49:32 +0200

    add pooling back

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,5 +1,5 @@
-// import { Pool as NeonPool } from '@neondatabase/serverless';
-import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { Pool } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/node-postgres';
 // import { Pool as PgPool } from 'pg';
 
 import * as schema from '@/db/schema';
@@ -7,12 +7,9 @@ import * as schema from '@/db/schema';
 // const Pool = import.meta.env.PROD ? NeonPool : PgPool;
 // const Pool = PgPool;
 
-// export const pool = new Pool({
-//   connectionString: import.meta.env.HOMEWISE_DATABASE_URL,
-// });
-
-export const db: NodePgDatabase<typeof schema> = drizzle(import.meta.env.HOMEWISE_DATABASE_URL, {
-  schema,
-  casing: 'snake_case',
+export const pool = new Pool({
+  connectionString: import.meta.env.HOMEWISE_DATABASE_URL,
 });
+
+export const db = drizzle(pool, { schema, casing: 'snake_case' });
 export { schema };
