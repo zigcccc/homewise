5805d49681489efa017e70dde0813bcb1a0b0b76
Žiga Krašovec <zkrasovec@gmail.com>
2026-07-15 08:52:08 +0200

    chore: Removed zod path alias

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,28 +1,15 @@
-import path from 'path';
-
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [
-    tsConfigPaths({ projects: ['./tsconfig.json'] }),
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
   server: { port: 3000 },
   resolve: {
-    alias: [
-      {
-        find: /zod\/v4\/core/,
-        replacement: path.resolve(__dirname, 'node_modules', 'zod', 'v4', 'core'),
-      },
-    ],
+    tsconfigPaths: true,
   },
 });
